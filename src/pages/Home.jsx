0d99e9200579a6eece6c5aebc4103effc5f14813
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-11 21:30:06 -0300

    feat: criação hero section , css e estilização global

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
 import Button from "../components/Button"
+import "./Home.css"
 import { useState } from "react"
 
 function Home(){
@@ -8,11 +9,12 @@ function Home(){
         <>
             <Header/>
 
-            <section>
+            <section className="home-hero-section">
                 <img src="" alt="" />
                 <h1>Beleza que expressa quem você é</h1>
                 <p>Unhas impecáveis, atendimento personalizado e uma experiência única para realçar sua essência.</p>
-                <div>
+                
+                <div className="section-buttons">
                   <Button variant="primary" element="a">Agendar horário</Button>
                   <Button variant="secundary" element="Link">Nossos serviços  →</Button>
                 </div>            
