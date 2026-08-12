4a075da3dc3949705f6aa34ef62b192dcce58d9b
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-11 22:22:06 -0300

    feat: estilização da home , hero section, imagem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header"
 import Button from "../components/Button"
 import "./Home.css"
 import { useState } from "react"
+import IMG from "../assets/images/IMG-HERO-SECTION.PNG"
 
 function Home(){
     
@@ -10,14 +11,21 @@ function Home(){
             <Header/>
 
             <section className="home-hero-section">
-                <img src="" alt="" />
-                <h1>Beleza que expressa quem você é</h1>
-                <p>Unhas impecáveis, atendimento personalizado e uma experiência única para realçar sua essência.</p>
+                <img 
+                  src={IMG}
+                  alt="imagem de uma mão com unhas em destaque pintadas de terracota" 
+                />
+
+                <div className="hero-content">
+                   <h1>Beleza que <br/> expressa quem você é</h1>
+                   <p>Unhas impecáveis, atendimento personalizado e uma experiência única para realçar sua essência.</p>
                 
-                <div className="section-buttons">
-                  <Button variant="primary" element="a">Agendar horário</Button>
-                  <Button variant="secundary" element="Link">Nossos serviços  →</Button>
-                </div>            
+                   <div className="section-buttons">
+                     <Button variant="primary" element="a">Agendar horário</Button>
+                     <Button variant="secundary" element="Link">Nossos serviços  →</Button>
+                   </div>   
+                </div>
+                          
             </section>
         </>
     )
