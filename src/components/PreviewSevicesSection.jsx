3a1205bed0a1e7b9b204016966e5fe0b5b1655cd
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-28 16:56:49 -0300

    feat: CSS do preview Services, espaçamentos do card, nova div para texto e link

--- src/components/PreviewSevicesSection.jsx
+++ src/components/PreviewSevicesSection.jsx
@@ -11,18 +11,24 @@ function PreviewServicesSection() {
             <h2>Nossos Serviços</h2>
             <div className="preview-services-card">
                 <img src={IMGM}  alt="Imagem de uma mao segurando esmalte azul da dailus com foco nas unhas pintadas" />
-                <h3>Manicure Perfeita</h3>
-                <Link to="/services">Saiba mais →</Link>
+                <div>
+                   <h3>Manicure Perfeita</h3>
+                   <Link to="/services">Saiba mais →</Link>
+                </div>
             </div>
-            <div className="-preview-ervices-card">
+            <div className="preview-services-card">
                 <img src={IMGP}  alt="Imagem de pès com destaque nas unhas cutiladas e esmaltadas com francesinha" />
-                <h3>Pedicure + Esfoliação</h3>
-                <Link to="/services">Saiba mais →</Link>
+                <div>
+                   <h3>Pedicure + Esfoliação</h3>
+                   <Link to="/services">Saiba mais →</Link>
+                </div>
             </div>
             <div className="preview-services-card">
                 <img src={IMGS}  alt="Imagem de um pé e um balse de agua" />
-                <h3>Spa dos pés</h3>
-                <Link to="/services">Saiba mais →</Link>
+                <div>
+                   <h3>Spa dos pés</h3>
+                   <Link to="/services">Saiba mais →</Link>
+                </div>
             </div>
 
             <Button variant="secondary" element="Link">VER TODOS SERVIÇOS →</Button>
