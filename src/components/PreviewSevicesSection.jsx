393715550487996409e3377ab40b0286f5ad74d9
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-28 17:30:22 -0300

    fix: quebra de texto Preview Services card

--- src/components/PreviewSevicesSection.jsx
+++ src/components/PreviewSevicesSection.jsx
@@ -12,21 +12,21 @@ function PreviewServicesSection() {
             <div className="preview-services-card">
                 <img src={IMGM}  alt="Imagem de uma mao segurando esmalte azul da dailus com foco nas unhas pintadas" />
                 <div>
-                   <h3>Manicure Perfeita</h3>
+                   <h3>Manicure <br/>Perfeita</h3>
                    <Link to="/services">Saiba mais →</Link>
                 </div>
             </div>
             <div className="preview-services-card">
                 <img src={IMGP}  alt="Imagem de pès com destaque nas unhas cutiladas e esmaltadas com francesinha" />
                 <div>
-                   <h3>Pedicure + Esfoliação</h3>
+                   <h3>Pedicure + <br/>Esfoliação</h3>
                    <Link to="/services">Saiba mais →</Link>
                 </div>
             </div>
             <div className="preview-services-card">
                 <img src={IMGS}  alt="Imagem de um pé e um balse de agua" />
                 <div>
-                   <h3>Spa dos pés</h3>
+                   <h3>Spa dos <br/>pés</h3>
                    <Link to="/services">Saiba mais →</Link>
                 </div>
             </div>
