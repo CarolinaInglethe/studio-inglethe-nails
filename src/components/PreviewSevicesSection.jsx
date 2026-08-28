3fa3d254550bc815bcf04837b05e798dc861345f
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-28 16:19:02 -0300

    feat: adiciona botao ver todos serviços no Preview services da home

--- src/components/PreviewSevicesSection.jsx
+++ src/components/PreviewSevicesSection.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Button from './Button';
 import IMGM from "../assets/images/IMG_8830.PNG"
 import IMGP from "../assets/images/IMG_8860.PNG"
 import IMGS from "../assets/images/IMG_8870.PNG"
@@ -10,19 +11,20 @@ function PreviewServicesSection() {
             <div className="preview-services-card">
                 <img src={IMGM} width="160px" alt="" />
                 <h3>Manicure Perfeita</h3>
-                <Link to="/services">Saiba mais </Link>
+                <Link to="/services">Saiba mais →</Link>
             </div>
             <div className="-preview-ervices-card">
                 <img src={IMGP} width="160px" alt="" />
                 <h3>Pedicure + Esfoliação</h3>
-                <Link to="/services">Saiba mais </Link>
+                <Link to="/services">Saiba mais →</Link>
             </div>
             <div className="preview-services-card">
                 <img src={IMGS} width="160px" alt="" />
                 <h3>Spa dos pés</h3>
-                <Link to="/services">Saiba mais </Link>
+                <Link to="/services">Saiba mais →</Link>
             </div>
 
+            <Button variant="secondary" element="Link">VER TODOS SERVIÇOS →</Button>
         </section>
     )
 }
