45b77831986632a3ff47e756417d2c852777bf3b
CarolinaInglethe <carolinainglethe@gmail.com>
2026-08-28 16:41:25 -0300

    feat: CSS do Preview Services, Display no componente

--- src/components/PreviewSevicesSection.jsx
+++ src/components/PreviewSevicesSection.jsx
@@ -3,23 +3,24 @@ import Button from './Button';
 import IMGM from "../assets/images/IMG_8830.PNG"
 import IMGP from "../assets/images/IMG_8860.PNG"
 import IMGS from "../assets/images/IMG_8870.PNG"
+import "./PreviewServicesSection.css"
 
 function PreviewServicesSection() {
     return (
         <section className="preview-services-section">
             <h2>Nossos Serviços</h2>
             <div className="preview-services-card">
-                <img src={IMGM} width="160px" alt="" />
+                <img src={IMGM}  alt="Imagem de uma mao segurando esmalte azul da dailus com foco nas unhas pintadas" />
                 <h3>Manicure Perfeita</h3>
                 <Link to="/services">Saiba mais →</Link>
             </div>
             <div className="-preview-ervices-card">
-                <img src={IMGP} width="160px" alt="" />
+                <img src={IMGP}  alt="Imagem de pès com destaque nas unhas cutiladas e esmaltadas com francesinha" />
                 <h3>Pedicure + Esfoliação</h3>
                 <Link to="/services">Saiba mais →</Link>
             </div>
             <div className="preview-services-card">
-                <img src={IMGS} width="160px" alt="" />
+                <img src={IMGS}  alt="Imagem de um pé e um balse de agua" />
                 <h3>Spa dos pés</h3>
                 <Link to="/services">Saiba mais →</Link>
             </div>
